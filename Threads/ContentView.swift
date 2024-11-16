dd2f9547a1428059452f5a2f0ea34b38ec43e3a6
Vinay Sandhu <developervsandhu@gmail.com>
2024-11-16 15:27:19 -0500

    Initial Commit

---
+++ Threads/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threads
+//
+//  Created by Vinay Sandhu on 2024-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Vinay Sandhu on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
